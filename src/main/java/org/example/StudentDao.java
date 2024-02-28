c977de9825c6947793631ce353a60df28e7267d4
shubhamp13 <shubhampuri8297@gmail.com>
2024-02-28 23:01:43 +0530

    changes

--- src/main/java/org/example/StudentDao.java
+++ src/main/java/org/example/StudentDao.java
@@ -3,8 +3,6 @@ package org.example;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
-import java.util.ArrayList;
 import java.util.List;
 
 public class StudentDao
